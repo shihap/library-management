2bb385a22602005b2b8aac1207340143a9b2f4fd
Shehab Ahmed <shihap.amara@gmail.com>
2024-12-24 02:18:42 +0200

    turn dependency injection to constructor injection

--- src/main/java/com/library/library_management/LibraryManagementApplication.java
+++ src/main/java/com/library/library_management/LibraryManagementApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryManagementApplication implements CommandLineRunner {
 
-	@Autowired // 2- spring will inject the created instance (bean) here
 	private HelloService helloService ; // (if no @Autowired) private HelloService helloService = new HelloService();
+
+	@Autowired // 2- spring will inject the created instance (bean) here
+	public LibraryManagementApplication(HelloService helloService){ // here we used contructor injection instead of field injection
+		this.helloService = helloService ; 
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
