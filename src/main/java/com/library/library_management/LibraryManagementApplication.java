640a1ee249900f439fd4fda8b07fc768eeaab29d
Shehab Ahmed <shihap.amara@gmail.com>
2024-12-29 22:13:19 +0200

    Added Logger in LibraryManagementApplication

--- src/main/java/com/library/library_management/LibraryManagementApplication.java
+++ src/main/java/com/library/library_management/LibraryManagementApplication.java
@@ -1,27 +1,31 @@
 package com.library.library_management;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryManagementApplication {
+public class LibraryManagementApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LibraryManagementApplication.class, args);
-    }
+    //will use it to create the logs for this class in the console and log file (logs/book.log)
+    private static final Logger log = LoggerFactory.getLogger(LibraryManagementApplication.class);
+
+    //the log pattern in logback.xml file , it can also written in properties file
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+
+	public void run(String... args) throws Exception{
+        log.trace("This is a trace message.");
+        log.debug("This is a debug message.");
+		log.info("This is an info message.");
+        log.warn("This is a warn message.");
+        log.error("This is an error message.");
+	}
 
 }
-
-/*
- * to access the H2 console : http://localhost:8080/h2-console
- * 
- * 
- * sql for some books:
- * 
-INSERT INTO BOOK (ID,AUTHOR, TITLE) VALUES 
-(1,'Harper Lee', 'To Kill a Mockingbird'),
-(2,'George Orwell', '1984'),
-(3,'J.D. Salinger', 'The Catcher in the Rye')
- * 
-*/
-
